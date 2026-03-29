b15e8a1749a9a833f7fc3af5aeef4d32fb2a23d2
enjoyy606 <enjoyy606@gmail.com>
2026-03-29 20:11:55 +0530

    Change SHORTENER_API and SHORTENER_WEBSITE values
    Updated SHORTENER_API and SHORTENER_WEBSITE environment variables.

--- info.py
+++ info.py
@@ -76,8 +76,8 @@ TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/")
 VERIFY_IMG = environ.get(
     "VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg"
 )
-SHORTENER_API = environ.get("SHORTENER_API", "3097623f852197a9ce40d1212aaa8bbf2803e799")
-SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "omegalinks.in")
+SHORTENER_API = environ.get("SHORTENER_API", "af2492502a6364145ea8c019324d4c14ff5d9c52")
+SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "shortxlinks.com")
 SHORTENER_API2 = environ.get(
     "SHORTENER_API2", "3097623f852197a9ce40d1212aaa8bbf2803e799"
 )
